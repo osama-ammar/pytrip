69dc747d1652408cb5263e3d3102dd96ae3304b2
Leszek Grzanka <grzanka@agh.edu.pl>
2020-06-03 20:55:59 +0200

    pep8 fixes

--- setup.py
+++ setup.py
@@ -25,64 +25,15 @@ import subprocess
 import sys
 from setuptools.command.build_ext import build_ext as _build_ext
 
-
-class build_ext(_build_ext):
-    """
-    From https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py/21621689#21621689
-    """
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
+from pytrip.version import git_version
 
 
-def git_version():
-    """
-    Inspired by https://github.com/numpy/numpy/blob/master/setup.py
-    :return: the git revision as a string
-    """
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'describe', '--tags', '--long'])
-        GIT_REVISION = out.strip().decode('ascii')
-        print('GIT_REVISION', GIT_REVISION)
-        if GIT_REVISION:
-            no_of_commits_since_last_tag = int(GIT_REVISION.split('-')[1])
-            tag_name = GIT_REVISION.split('-')[0][1:]
-            if no_of_commits_since_last_tag == 0:
-                version = tag_name
-            else:
-                version = '{}+rev{}'.format(tag_name, no_of_commits_since_last_tag)
-        else:
-            version = "Unknown"
-    except OSError:
-        version = "Unknown"
-
-    return version
-
-
-def write_version_py(filename='pytrip/__init__.py'):
-    cnt = """
-__version__ = '%(version)s'
+def write_version_py(filename=os.path.join('pytrip', 'VERSION')):
+    cnt = """%(version)s
 """
 
     GIT_REVISION = git_version()
-    a = open(filename, 'a')
+    a = open(filename, 'w')
     try:
         a.write(cnt % {'version': GIT_REVISION})
     finally:
